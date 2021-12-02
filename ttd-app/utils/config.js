d4b56fd3ee6b5b2b778536cf0f5ec6fbdf372219
vsboo <vela.a.steve@gmail.com>
2021-12-02 15:27:39 +0800

    config

--- ttd-app/utils/config.js
+++ ttd-app/utils/config.js
@@ -1,10 +1,10 @@
 
-const debug = false;
+const debug = true;
 export default {
 	
-	baseUrl:debug ? 'http://139.196.182.247:8080' : 'https://ttuodi.com',
+	baseUrl:debug ? 'http://150.158.41.165:8080' : 'https://ttuodi.com',
 	// 微信id
-	appId:'wxed5f92cfcafbc8c1',
+	appId:'wx2946b9fe324cb1e4',
 	// 本地存储的key值
 	storageKeys:{
 		// 登录用户的信息
