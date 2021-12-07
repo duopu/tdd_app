c54914e5dea00ff173b3e5bc118ae1931c0c3a75
vsboo <vela.a.steve@gmail.com>
2021-12-08 02:55:28 +0800

    订单投诉

--- ttd-app/pages/receive-order/complaint/complaint.vue
+++ ttd-app/pages/receive-order/complaint/complaint.vue
@@ -7,11 +7,12 @@
       <view class="complaint">
         <view class="complaint-text">评价内容：</view>
         <view class="complaint-content">
-					<input :value="content" @input="onInput()" placeholder="可以的话，多少写点！\n方便工作人员快速排队故障。可以的话，多少写点！方便工作人员快速排队故障。" />
-          
+					<input :value="content" @input="onInput" placeholder="可以的话，多少写点！\n方便工作人员快速排队故障。可以的话，多少写点！方便工作人员快速排队故障。" />
         </view>
-
-        <upload-list upload-text="添加照片" :img-list="[1, 2]" />
+				
+        <upload-list upload-text="添加照片" :img-list="imageList" @upload="chooseImage"/>
+				
+         <upload-list upload-icon="2" @upload="chooseFile"/>
       </view>
 
     </back-container>
@@ -24,6 +25,7 @@
 </template>
 <script>
 	import BackContainer from "../../mine/addressManage/component/backContainer";
+	import AddRemark from "../component/addRemark";
   import IphonexBottom from "../../mine/addressManage/component/iphonexBottom";
   import BigBtn from "../../mine/addressManage/component/bigBtn";
   import UploadList from "../component/uploadList";
@@ -32,17 +34,58 @@
     components: { UploadList, BigBtn, IphonexBottom, BackContainer },
     data() {
 			return {
+				id: '',
 				content: '',
+				fileList: [],
 				imageList: [],
 			};
+		},
+		onLoad(option) {
+			if (option.id) {
+				this.id = option.id;
+			}
 		},
 		methods: {
 			onInput(e) {
 				this.content = e.target.value;
 			},
+			chooseImage() {
+				uni.chooseImage({
+				    sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+				    success: (res) => {
+								const path = res.tempFilePaths[0];
+								this.uploadImage(path, 1);
+				    }
+				});
+			},
+			chooseFile() {
+				wx.chooseMessageFile({
+				  count: 1,
+					success: function (res) {
+				    const path = res.tempFilePaths[0];
+				    this.uploadImage(path, 2);
+				  }
+				});
+			},
+			uploadImage(path, type) {
+				const param = {
+					file: path,
+				};
+				this.$http.upload({ path }, true)
+				.then(res=>{
+					if (type == 1) {
+						this.imageList.push(res);
+					} else {
+						this.fileList.push(res);
+					}
+				});
+			},
 			commitComplaint() {
 				const params = {
 					detail: this.content,
+					fileList: this.fileList,
+					picList: this.imageList,
+					receiveOrderId: this.id,
 				};
 				this.$http.post('/b/ordercomplain/add', params, true)
 				.then(res => {
