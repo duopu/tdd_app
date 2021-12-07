264f379a148a1611b43f34d00a88671ee34c450c
jiangeq <jiangeq>
2021-12-07 13:59:04 +0800

    地址管理 - 新加 新建地址按钮

--- ttd-app/pages/mine/addressManage/addressManage.vue
+++ ttd-app/pages/mine/addressManage/addressManage.vue
@@ -2,57 +2,66 @@
   <view class="address-manage">
     <custom-navbar title="地址管理" />
     <back-container>
-	  <view>
-		<address-item v-for="(item, index) in addressList" :key="index" :address="item" @onClick="itemClick" @rightClick="rightClick" />
-	  </view>
-	</back-container>
+      <view>
+        <address-item v-for="(item, index) in addressList" :key="index" :address="item" @onClick="itemClick"
+                      @rightClick="rightClick" />
+      </view>
+    </back-container>
+
+    <iphonex-bottom>
+      <big-btn button-text="新增地址" />
+    </iphonex-bottom>
+
   </view>
 </template>
 
 <script>
 import BackContainer from './component/backContainer';
 import AddressItem from './component/addressItem';
+import IphonexBottom from "./component/iphonexBottom";
+import BigBtn from "./component/bigBtn";
 
 export default {
-  components: { AddressItem, BackContainer },
+  components: { BigBtn, IphonexBottom, AddressItem, BackContainer },
   data() {
-	  return {
-			isSelect: false,
-	    addressList: []
-	  };
+    return {
+      isSelect: false,
+      addressList: []
+    };
   },
-	onLoad(option) {
-		if (option.isSelect) {
-			this.isSelect = option.isSelect == '1';
-		}
-	},
-  onReady() {},
+  onLoad(option) {
+    if (option.isSelect) {
+      this.isSelect = option.isSelect == '1';
+    }
+  },
+  onReady() {
+  },
   onShow() {
-	  this.queryAddressData();
+    this.queryAddressData();
   },
   methods: {
-		itemClick(address) {
-			if (this.isSelect) {
-				
-				// 传递选择的地址
-				const eventChannel = this.getOpenerEventChannel();
-				eventChannel.emit('onSelect', address);
-				
-				uni.navigateBack({});
-				
-			} else {
-				uni.navigateTo({ url: `/pages/mine/editAddress/editAddress?id=${address.id}` });
-			}
-		},
-	  rightClick(id) {
-	    uni.navigateTo({ url: `/pages/mine/editAddress/editAddress?id=${id}` });
-	  },
-	  queryAddressData() {
-	    this.$http.post('/b/customeraddress/queryPageList', { pageSize: 1000 }, true)
-		  .then(res => {
-		    this.addressList = res.dataList;
-		  })
-	  }
+    itemClick(address) {
+      if (this.isSelect) {
+
+        // 传递选择的地址
+        const eventChannel = this.getOpenerEventChannel();
+        eventChannel.emit('onSelect', address);
+
+        uni.navigateBack({});
+
+      } else {
+        uni.navigateTo({ url: `/pages/mine/editAddress/editAddress?id=${address.id}` });
+      }
+    },
+    rightClick(id) {
+      uni.navigateTo({ url: `/pages/mine/editAddress/editAddress?id=${id}` });
+    },
+    queryAddressData() {
+      this.$http.post('/b/customeraddress/queryPageList', { pageSize: 1000 }, true)
+          .then(res => {
+            this.addressList = res.dataList;
+          })
+    }
   }
 }
 </script>
