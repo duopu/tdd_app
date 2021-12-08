6288bbd1937db809148fe8794238594995b68ea6
vsboo <vela.a.steve@gmail.com>
2021-12-09 02:16:21 +0800

    订单详情

--- ttd-app/pages/receive-order/component/quotedIten.vue
+++ ttd-app/pages/receive-order/component/quotedIten.vue
@@ -1,8 +1,8 @@
 <template>
   <view class="offer-2">
     <view class="offer-21">联系人：{{ order.orderAddress.name }} {{ order.orderAddress.phone }}</view>
-    <view class="offer-21">报价周期：{{ order.quotationStart.slice(0, 10) }} 至 {{ order.quotationEnd.slice(0, 10) }}</view>
-    <view class="offer-21">工作周期：{{ order.workStart.slice(0, 10) }} 至 {{ order.workEnd.slice(0, 10) }}</view>
+    <view class="offer-21">报价周期：{{ (order.quotationStart || '').slice(0, 10) }} 至 {{ (order.quotationEnd || '').slice(0, 10) }}</view>
+    <view class="offer-21">工作周期：{{ (order.workStart || '').slice(0, 10) }} 至 {{ (order.workEnd || '').slice(0, 10) }}</view>
     <view class="offer-21">工作地址：{{ order.orderAddress.province }} {{ order.orderAddress.city }} {{ order.orderAddress.district }} {{ order.orderAddress.address }}</view>
     <view class="offer-21">工作内容：{{ order.detail }}</view>
     <view v-if="order.remark" class="offer-21">备注：{{ order.remark }}</view>
