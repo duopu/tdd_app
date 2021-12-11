5fcab40e1bd1c77f2b38c81186f2d4f57562632f
jiangeq <jiangeq>
2021-12-11 09:17:36 +0800

    新增时间修改

--- ttd-app/pages/mine/aboutUs/consultingModel.vue
+++ ttd-app/pages/mine/aboutUs/consultingModel.vue
@@ -1,18 +1,51 @@
 <template>
-  <view class="cons-model">
-    <view class="cons-model-back"/>
+  <view class="cons-model" v-if="visible">
+    <view class="cons-model-back" @click="hide" />
     <view class="cons-model-bottom">
+      <view class="cmb-title">
+        <view class="cmb-title-left"></view>
+        <view class="cmb-title-middle"></view>
+        <view class="cmb-title-right"></view>
+      </view>
 
+      view.
     </view>
   </view>
 </template>
 <script>
 export default {
-  name: "consultingModel"
+  name: "consultingModel",
+  data() {
+    return {
+      visible: false,
+    }
+  },
+  methods: {
+    show() {
+      this.visible = true;
+    },
+    hide() {
+      this.visible = false;
+    },
+  }
 }
 </script>
 <style scoped lang="scss">
 .cons-model {
+  width: 750rpx;
+  height: 100vh;
+  position: fixed;
+  z-index: 102;
+  left: 0;
+  top: 0;
+  display: flex;
+  flex-flow: column wrap;
 
+  .cons-model-back {
+    flex: 1;
+  }
+  .cons-model-bottom {
+
+  }
 }
 </style>
