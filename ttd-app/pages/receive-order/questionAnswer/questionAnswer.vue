5af09bd84b25368d543ef3337ef1625f0adb5455
vsboo <vela.a.steve@gmail.com>
2021-12-13 01:39:55 +0800

    问题列表

--- ttd-app/pages/receive-order/questionAnswer/questionAnswer.vue
+++ ttd-app/pages/receive-order/questionAnswer/questionAnswer.vue
@@ -4,17 +4,17 @@
 
     <back-container>
       <view class="qus-detail">
-        <view class="qus-it" v-for="i in 2" :key="i">
+        <view class="qus-it" v-for="question in questionList" :key="question.id.toString()">
           <view class="qus-1">
             <image :src="MDicon" class="qus-1image" />
             <view class="qus-1right">
               <view class="qus-1right-top">
-                <text class="qus-1tag">交换机</text>
-                <text class="qus-1time">2021-08-26 12:40</text>
+                <text class="qus-1tag">{{ question.questionJob }}</text>
+                <text class="qus-1time">{{ question.addTime }}</text>
               </view>
-              <view class="qus-1content">问题</view>
+              <view class="qus-1content">{{ question.content }}</view>
               <view class="qus-1img-list">
-                <image :src="MDicon" class="qus-1img-item" v-for="i in 7" :key="i" />
+                <image :src="p" class="qus-1img-item" v-for="p in question.pictureList" :key="p" />
               </view>
             </view>
           </view>
@@ -24,17 +24,17 @@
             <view class="qus-1right">
               <view class="qus-1right-top">
                 <text class="qus-1tag qus-2tag">发单者回复</text>
-                <text class="qus-1time">2021-08-26 12:40</text>
+                <text class="qus-1time">{{ question.answerList[0].addTime || '' }}</text>
               </view>
-              <view class="qus-2text">可以的话，多少写点！ 方便工作人员快速排队故障。</view>
+              <view v-if="question.answerList.length > 0" class="qus-2text">{{ question.answerList[0].content || '' }}</view>
               <view class="qus-1img-list">
-                <template v-if="i === 1">
-                  <image :src="MDicon" class="qus-1img-item" v-for="i in 7" :key="i" />
+                <template v-if="question.answerList.length > 0 && question.answerList[0].pictureList.length > 0">
+                  <image :src="p" class="qus-1img-item" v-for="p in question.answerList[0].pictureList" :key="p" />
                 </template>
-                <view class="qus-1img-empty" v-else >
+                <view class="qus-1img-empty" v-if="question.answerList.length == 0" >
                   <image src="https://ttd-public.obs.cn-east-3.myhuaweicloud.com/app-img/mine/empty-icon-11.svg" class="qus-1img-empty-img" />
                   <view class="qus-1img-empty-text">目前没有回答</view>
-                  <view class="qus-1img-empty-btn" @click="answerQuestion()">回答</view>
+									<view class="qus-1img-empty-btn" @click="answerQuestion(question)">回答</view>
                 </view>
               </view>
             </view>
@@ -43,7 +43,7 @@
       </view>
     </back-container>
 
-    <iphonex-bottom z-index="99">
+    <iphonex-bottom v-if="!isPlaceOrder" z-index="99">
       <big-btn button-text="我要提问" @click="$refs.askQuestionsModel.show()" />
     </iphonex-bottom>
 
@@ -140,22 +140,25 @@ export default {
 				url:`/pages/main/apply/tree?type=${type}`
 			});
 		},
-		answerQuestion(q) {
-			this.questionId = q.id;
+		answerQuestion(id) {
+			console.log('question id ', id);
+			this.questionId = id;
 			this.$refs.askQuestionsModel.show();
 		},
 		confirmQuestion(p) {
 			let url = `/b/orderquestionanswer/${this.isPlaceOrder ? 'answer' : 'put'}Question`;
 			const params = {
 				...p,
-				questionId: this.questionId,
+				// questionId: this.questionId,
+				questionId: 1006758008557696,
 				receiveOrderId: this.id,
 			};
-			this.$http.post('/b/orderquestionanswer/putQuestion', params, true)
+			this.$http.post(url, params, true)
 			.then(res => {
 				uni.showToast({
 					title: `${this.isPlaceOrder ? '回复' : '咨询'}已提交`,
 					success: () => {
+            this.$refs.askQuestionsModel.hide();
 						this.queryQuestionList();
 					}
 				});
@@ -272,17 +275,17 @@ export default {
           font-weight: 400;
           color: #828282;
         }
-        .qus-1img-empty-btn {
-          width: 80rpx;
-          height: 48rpx;
-          border-radius: 32rpx;
-          border: 2rpx solid #EDEDED;
-          font-size: 24rpx;
-          font-family: PingFang SC-Regular, PingFang SC;
-          font-weight: 400;
-          color: #4F4F4F;
-          @include flexCenter;
-        }
+				.qus-1img-empty-btn {
+				  width: 80rpx;
+				  height: 48rpx;
+				  border-radius: 32rpx;
+				  border: 2rpx solid #EDEDED;
+				  font-size: 24rpx;
+				  font-family: PingFang SC-Regular, PingFang SC;
+				  font-weight: 400;
+				  color: #4F4F4F;
+				  @include flexCenter;
+				}
       }
     }
   }
