c34f03456f988a3c6c5b2beb4bd1bdfe0103cb6d
DL <18911755085@163.com>
2021-12-27 14:40:54 +0800

    抽奖样式

--- ttd-app/pages/mine/lotteryCenter/lotteryCenter.vue
+++ ttd-app/pages/mine/lotteryCenter/lotteryCenter.vue
@@ -1,275 +1,286 @@
 <template>
-  <view>
+	<view class="lottery-page">
+		<custom-navbar title="抽奖中心" />
+		<view>
+			<view class="luck-draw-view">
+				<view class="wheel-box-det" @click="checkRule">查看规则</view>
+				<LuckyWheel class="LuckyWheel" ref="myLucky" width="560rpx" height="560rpx" :blocks="blocks" :prizes="prizes"
+					:buttons="buttons" :defaultStyle="defaultStyle" @start="startCallBack" @end="endCallBack" />
 
-    <custom-navbar title="抽奖中心" />
+				<view class="count-text">
+					你还有
+					<text class="count-num-text">{{ lotteryCount }}</text>
+					次抽奖机会
+				</view>
+			</view>
 
-    <view>
-      <view>
-        <view class="wheel-box-det" @click="checkRule">查看规则</view>
-
-
-        <!-- <hxr-wheel @start="start" ref="hxrWheel" />-->
-
-        <view style="width: 300rpx;height: 300rpx;border: 1rpx solid black;">llllll</view>
-
-        <!--https://100px.net/usage/uni-app.html   https://100px.net/demo/wheel.html  -->
-        <LuckyWheel
-            ref="myLucky"
-            width="600rpx"
-            height="600rpx"
-            :blocks="blocks"
-            :prizes="prizes"
-            :buttons="buttons"
-            :defaultStyle="defaultStyle"
-            @start="startCallBack"
-            @end="endCallBack"
-        />
-
-        <view style="width: 300rpx;height: 300rpx;border: 1rpx solid black;">llllll</view>
-        <view style="width: 300rpx;height: 300rpx;border: 1rpx solid black;">llllll</view>
-        <view style="width: 300rpx;height: 300rpx;border: 1rpx solid black;">llllll</view>
-
-        <view class="wheel-box-det1">
-          你还有
-          <text class="wheel-box-det2">{{ lotteryCount }}</text>
-          次抽奖机会
-        </view>
-      </view>
-
-      <view class="wheel-boo0">
-        <view class="wheel-boo1">中奖记录</view>
-        <view class="wheel-boo2" v-for="i in lotteryList" :key="i.id">
-          <view class="wheel-boo3">
-            <view class="wheel-boo31">{{ i.prize }}</view>
-            <view class="wheel-boo32">{{ i.prizeTime }}</view>
-          </view>
-          <view class="wheel-boo4">{{ i.state == 1 ? '未发放' : i.state == 2 ? '物流快递中' : '已发放'}}</view>
-        </view>
+			<view class="wheel-boo0">
+				<view class="wheel-boo1">中奖记录</view>
+				<view class="wheel-boo2" v-for="i in lotteryList" :key="i.id">
+					<view class="wheel-boo3">
+						<view class="wheel-boo31">{{ i.prize }}</view>
+						<view class="wheel-boo32">{{ i.prizeTime }}</view>
+					</view>
+					<view class="wheel-boo4">{{ i.state == 1 ? '未发放' : i.state == 2 ? '物流快递中' : '已发放'}}</view>
+				</view>
 
 				<list-empty v-if="!lotteryList.length" />
-      </view>
+			</view>
 
-    </view>
-  </view>
+		</view>
+	</view>
 </template>
 
 <script>
-import HxrWheel from "./hxrWheel";
-import ListEmpty from "../../place-order/orderList/listEmpty";
-import LuckyWheel from '@lucky-canvas/uni/lucky-wheel' // 大转盘
+	import HxrWheel from "./hxrWheel";
+	import ListEmpty from "../../place-order/orderList/listEmpty";
+	import LuckyWheel from '@lucky-canvas/uni/lucky-wheel' // 大转盘
 
-export default {
-  name: "lotteryCenter",
-  components: { ListEmpty, HxrWheel, LuckyWheel },
-  data() {
-    return {
-
-      blocks: [{ padding: '13px', background: '#FF5B2D' }],
-      prizes: [
-        { fonts: [{ text: '0', top: '10%' }], background: '#50CA12' },
-        { fonts: [{ text: '1', top: '10%' }], background: '#FFB42C' },
-        { fonts: [{ text: '2', top: '10%' }], background: '#50CA12' },
-        { fonts: [{ text: '3', top: '10%' }], background: '#FFB42C' },
-        { fonts: [{ text: '4', top: '10%' }], background: '#50CA12' },
-        { fonts: [{ text: '5', top: '10%' }], background: '#FFB42C' },
-        { fonts: [{ text: '6', top: '10%' }], background: '#50CA12' },
-        { fonts: [{ text: '7', top: '10%' }], background: '#FFB42C' },
-      ],
-      defaultStyle: {
-        position: 'relative',
-        left: '100rpx',
-        top: '200rpx'
-      },
-      buttons: [
-        {
-          radius: '50%',
-          imgs: [{
-            src: 'https://ttd-public.obs.cn-east-3.myhuaweicloud.com/app-img/mine/s-dial_pointer.png',
-            width: '100%',
-            top: '-100%'
-          }]
-        }
-      ],
-
-
-			lotteryCount: 0,
-			prizeList: [],
-			lotteryList: [],
-		};
-  },
-	onShow() {
-		this.refresh();
-	},
-  methods: {
-
-    // 点击抽奖按钮触发回调
-    startCallBack () {
-      // 先开始旋转
-      this.$refs.myLucky.play()
-      // 使用定时器来模拟请求接口
-      setTimeout(() => {
-        // 假设后端返回的中奖索引是0
-        const index = 0
-        // 调用stop停止旋转并传递中奖索引
-        this.$refs.myLucky.stop(2)
-      }, 3000)
-    },
-    // 抽奖结束触发回调
-    endCallBack (prize) {
-      // 奖品详情
-      console.log(prize)
-    },
-
-
-		refresh() {
-			this.queryLotteryCount();
-			this.queryPriceList();
-			this.queryLotteryList();
+	export default {
+		name: "lotteryCenter",
+		components: {
+			ListEmpty,
+			HxrWheel,
+			LuckyWheel
 		},
-		queryLotteryCount() {
-			this.$http.post('/b/lottery/remainLotteryNum', {})
-				.then(res => {
-					this.lotteryCount = res || 0;
+		data() {
+			return {
+
+				blocks: [{
+					padding: '6px',
+					background: '#FF5B2D'
+				}],
+				prizes: [],
+				defaultStyle: {
+					position: 'relative',
+					left: '100rpx',
+					top: '200rpx'
+				},
+				buttons: [{
+					radius: '50%',
+					imgs: [{
+						src: 'https://ttd-public.obs.cn-east-3.myhuaweicloud.com/app-img/mine/s-dial_pointer.png',
+						width: '140rpx',
+						height: '140rpx',
+						top:'-70rpx'
+					}]
+				}],
+				lotteryCount: 0,
+				lotteryList: [],
+			};
+		},
+		onShow() {
+			this.refresh();
+		},
+		methods: {
+
+			// 点击抽奖按钮触发回调
+			startCallBack() {
+				// 先开始旋转
+				this.$refs.myLucky.play()
+				// 使用定时器来模拟请求接口
+				setTimeout(() => {
+					// 假设后端返回的中奖索引是0
+					const index = 0
+					// 调用stop停止旋转并传递中奖索引
+					this.$refs.myLucky.stop(2)
+				}, 3000)
+			},
+			// 抽奖结束触发回调
+			endCallBack(prize) {
+				// 奖品详情
+				console.log(prize)
+			},
+			refresh() {
+				this.queryLotteryCount();
+				this.queryPriceList();
+				this.queryLotteryList();
+			},
+			// 查询剩余抽奖次数
+			queryLotteryCount() {
+				this.$http.post('/b/lottery/remainLotteryNum', {})
+					.then(res => {
+						this.lotteryCount = res || 0;
+					})
+			},
+			// 查询奖品列表
+			queryPriceList() {
+				this.$http.post('/b/lottery/prizeList', {})
+					.then(res => {
+						this.prizes = res.map((pri,index)=>{
+							const background = index % 2 == 1 ? '#50CA12' : '#FFB42C'
+							return {
+								range:pri.point,
+								background,
+								id:pri.id,
+								fonts:[{
+									text:pri.name,
+									top:'10px',
+									fontColor:'#ffffff',
+									fontSize:'16px',
+									wordWrap:false
+									}],
+								imgs:[{
+									src:pri.icon || 'https://ttd-public.obs.cn-east-3.myhuaweicloud.com/app-img/mine/my_icon_wallet.png',
+									width:'30px',
+									height:'30px',
+									top:'40px'
+								}]
+							}
+						});
+						console.log('bkb',this.prizes);
+					})
+					
+					
+			},
+			// 查询中奖记录
+			queryLotteryList() {
+				this.$http.post('/b/lottery/queryPageList', {
+						pageSize: 100,
+						sortInfos: [{
+							field: 'prizeTime',
+							sort: 'desc',
+						}]
+					})
+					.then(res => {
+						
+						this.lotteryList = res.dataList || [];
+					})
+			},
+			checkRule() {
+				uni.showModal({
+					title: '抽奖规则',
+					content: '给我中！给我中！给我中！给我中！给我中！给我中！给我中！给我中！给我中！',
+					showCancel: false,
+					confirmText: '我知道了',
 				})
-		},
-		queryPriceList() {
-			this.$http.post('/b/lottery/prizeList', {})
-				.then(res => {
-					this.prizeList = res || [];
-				})
-		},
-		queryLotteryList() {
-			this.$http.post('/b/lottery/queryPageList', {
-				pageSize: 100,
-				sortInfos: [{
-					field: 'prizeTime',
-					sort: 'desc',
-				}]
-			})
-				.then(res => {
-					this.lotteryList = res.dataList || [];
-				})
-		},
-		checkRule() {
-			uni.showModal({
-				title: '抽奖规则',
-				content: '给我中！给我中！给我中！给我中！给我中！给我中！给我中！给我中！给我中！',
-				showCancel: false,
-				confirmText: '我知道了',
-			})
-		},
-    start() {
-      this.$refs.hxrWheel.begin(4);
-    },
-		startLottery() {
-			this.$http.post('/b/lottery/startLottery', {})
-				.then(res => {
-				})
-		},
-  }
-}
+			},
+			start() {
+				this.$refs.hxrWheel.begin(4);
+			},
+			startLottery() {
+				this.$http.post('/b/lottery/startLottery', {})
+					.then(res => {})
+			},
+		}
+	}
 </script>
 
 <style lang="scss">
-.wheel-box {
-  background-image: url("https://ttd-public.obs.cn-east-3.myhuaweicloud.com/app-img/mine/lottery-back.png");
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  padding: 100rpx 36rpx;
-  box-sizing: border-box;
+	.lottery-page {
 
-  .wheel-box-det {
-    font-size: 28rpx;
-    font-family: PingFang SC-Regular, PingFang SC;
-    font-weight: 400;
-    color: #FFFFFF;
-    line-height: 36rpx;
-    text-align: right;
-  }
-
-  .wheel-box-det1 {
-    width: 342rpx;
-    height: 76rpx;
-    @include flexCenter;
-    margin: 32rpx auto 0 auto;
-    background: rgba(255, 255, 255, 0.6);
-    border-radius: 200rpx;
-
-    font-size: 28rpx;
-    font-family: PingFang SC-Regular, PingFang SC;
-    font-weight: 400;
-    color: #323335;
-
-    .wheel-box-det2 {
-      padding: 0 16rpx;
-      display: inline-block;
-      color: #FF3B30;
-    }
-  }
-}
-
-.wheel-boo0 {
-  background: white;
-  padding-top: 16rpx;
-  margin-bottom: 68rpx;
-
-  .wheel-boo1 {
-    padding: 0 32rpx;
-    height: 88rpx;
-    font-size: 28rpx;
-    display: flex;
-    align-items: center;
-    font-family: PingFang SC-Regular, PingFang SC;
-    font-weight: 400;
-    color: #828282;
-  }
-
-  .wheel-boo2 {
-    padding: 32rpx 32rpx 16rpx 0;
-    border-top: 1rpx solid #EDEDED;
-    margin-left: 32rpx;
-    display: flex;
-    align-items: center;
-
-
-    .wheel-boo3 {
-      flex: 1;
-
-      .wheel-boo31 {
-        font-size: 28rpx;
-        font-family: PingFang SC-Regular, PingFang SC;
-        font-weight: 400;
-        color: #323335;
-        line-height: 36rpx;
-      }
-
-      .wheel-boo32 {
-        margin-top: 16rpx;
-        font-size: 24rpx;
-        font-family: PingFang SC-Regular, PingFang SC;
-        font-weight: 400;
-        color: #828282;
-        line-height: 32rpx;
-      }
-    }
-
-    .wheel-boo4 {
-      width: 116rpx;
-      @include flexCenter;
-      height: 48rpx;
-      background: #FF9500;
-      border-radius: 8rpx;
-      font-size: 28rpx;
-      font-family: PingFang SC-Regular, PingFang SC;
-      font-weight: 400;
-      color: #FFFFFF;
-    }
-  }
-}
-</style>
-<style>
-page {
-  background: #F6F7F7;
-  overflow-y: scroll;
-}
+	}
+	.flex{
+		display: flex;
+		box-sizing: border-box;
+		position: relative;
+		flex-direction: column;
+		align-items: stretch;
+	}
+	
+	.luck-draw-view{
+		@extend  .flex;
+		height: 870rpx;
+		background-image: url('https://ttd-public.obs.cn-east-3.myhuaweicloud.com/app-img/mine/luck-draw-bg.png') ;
+		background-size: 750rpx 870rpx;
+		background-repeat: no-repeat;
+		
+		.wheel-box-det {
+			font-size: 28rpx;
+			position: absolute;
+			top: 50rpx;
+			right: 30rpx;
+			font-family: PingFang SC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #FFFFFF;
+			line-height: 36rpx;
+		}
+		
+		.LuckyWheel{
+			align-self: center;
+			margin-top: 100rpx;
+		}
+		
+		.count-text{
+			@extend  .flex;
+			width: 340rpx;
+			height: 76rpx;
+			border-radius: 38rpx;
+			background-color: #ffffff99;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+			margin-top: 20rpx;
+			align-self: center;
+			color: #333333;
+			font-size: 28rpx;
+			
+			.count-num-text{
+				color: #FF3B30;
+				font-size: 36rpx;
+				font-weight: bold;
+				margin-left: 10rpx;
+				margin-right: 10rpx;
+			}
+		}
+	}
+	
+	.wheel-boo0 {
+		background: white;
+		padding-top: 16rpx;
+		margin-bottom: 68rpx;
+	
+		.wheel-boo1 {
+			padding: 0 32rpx;
+			height: 88rpx;
+			font-size: 28rpx;
+			display: flex;
+			align-items: center;
+			font-family: PingFang SC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #828282;
+		}
+	
+		.wheel-boo2 {
+			padding: 32rpx 32rpx 16rpx 0;
+			border-top: 1rpx solid #EDEDED;
+			margin-left: 32rpx;
+			display: flex;
+			align-items: center;
+	
+	
+			.wheel-boo3 {
+				flex: 1;
+	
+				.wheel-boo31 {
+					font-size: 28rpx;
+					font-family: PingFang SC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #323335;
+					line-height: 36rpx;
+				}
+	
+				.wheel-boo32 {
+					margin-top: 16rpx;
+					font-size: 24rpx;
+					font-family: PingFang SC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #828282;
+					line-height: 32rpx;
+				}
+			}
+	
+			.wheel-boo4 {
+				width: 116rpx;
+				@include flexCenter;
+				height: 48rpx;
+				background: #FF9500;
+				border-radius: 8rpx;
+				font-size: 28rpx;
+				font-family: PingFang SC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #FFFFFF;
+			}
+		}
+	}
 </style>
