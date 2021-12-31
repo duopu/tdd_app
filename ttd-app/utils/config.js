91b92367212913b7dc58e228731ed6c51a265639
DL <18911755085@163.com>
2021-12-31 13:09:01 +0800

    修改线上环境域名

--- ttd-app/utils/config.js
+++ ttd-app/utils/config.js
@@ -1,7 +1,7 @@
 
 const debug = true;
 export default {
-	baseUrl:debug ? 'http://150.158.41.165:8080' : 'https://ttuodi.com',
+	baseUrl:debug ? 'http://150.158.41.165:8080' : 'https://api.ttuodi.com',
 	// 微信id
 	appId: debug ? 'wxa191a75756adc164': 'wxed5f92cfcafbc8c1' ,
 	// 本地存储的key值
