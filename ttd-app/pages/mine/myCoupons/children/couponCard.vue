09e95ede7b1d2c7816a141fce8a2a56fe9d23c3e
DL <18911755085@163.com>
2022-01-22 13:31:28 +0800

    优惠券  全部可用状态

--- ttd-app/pages/mine/myCoupons/children/couponCard.vue
+++ ttd-app/pages/mine/myCoupons/children/couponCard.vue
@@ -1,241 +1,248 @@
 <template>
-  <view class="coupon-card">
+	<view class="coupon-card">
 
-    <view class="c-card1">
-      <view class="c-card1-money">{{ coupon.couponNature == '3' ? `${coupon.discount}折` : coupon.parvalue / 100 }}</view>
-      <view class="c-card1-mj" v-if="coupon.couponNature != '2' ">
+		<view class="c-card1">
+			<view class="c-card1-money">{{ coupon.couponNature == '3' ? `${coupon.discount}折` : coupon.parvalue / 100 }}
+			</view>
+			<view class="c-card1-mj" v-if="coupon.couponNature != '2' ">
 				{{ coupon.couponNature == 1
 				 ? `满${ coupon.useMinPrice / 100 }减${ coupon.parvalue / 100 }`
 				 : `最高${ coupon.useMaxPrice / 100 }`
 				 }}
 			</view>
-    </view>
+		</view>
 
-    <view class="c-card2">
-      <view class="c-card21">
-        <view class="c-card24" :class="coupon.couponNature == '2' ? 'c-card24-red' : ''">{{ couponType(coupon.couponNature) }}</view>
-        <view class="c-card25">{{ coupon.title }}</view>
-      </view>
-      <view class="c-card22">{{ orderType(coupon.orderType) }}可用</view>
-      <view>
-        <view class="c-card23">
-          <text class="c-card23-text">{{ coupon.effectTime }} - {{ coupon.expireTime }}</text>
-          <uni-icons :type="spread ? 'arrowup' : 'arrowdown'" color="#BDBDBD" size="12" @click="changeSpread" />
-        </view>
-        <text v-if="spread" class="c-card23-text">{{ coupon.remark }}</text>
-      </view>
-    </view>
+		<view class="c-card2">
+			<view class="c-card21">
+				<view class="c-card24" :class="coupon.couponNature == '2' ? 'c-card24-red' : ''">
+					{{ couponType(coupon.couponNature) }}</view>
+				<view class="c-card25">{{ coupon.title }}</view>
+			</view>
+			<view class="c-card22">{{ orderType(coupon.orderType) }}可用</view>
+			<view>
+				<view class="c-card23">
+					<text class="c-card23-text">{{ coupon.effectTime }} - {{ coupon.expireTime }}</text>
+					<uni-icons :type="spread ? 'arrowup' : 'arrowdown'" color="#BDBDBD" size="12"
+						@click="changeSpread" />
+				</view>
+				<text v-if="spread" class="c-card23-text">{{ coupon.remark }}</text>
+			</view>
+		</view>
 
-    <view class="c-card3">
-      <view class="c-card31" />
-      <view class="c-card32" />
-      <view class="c-card33" />
-    </view>
+		<view class="c-card3">
+			<view class="c-card31" />
+			<view class="c-card32" />
+			<view class="c-card33" />
+		</view>
 
-   <view v-if="minusType == 1" class="c-card4" @click="$emit('useCoupon', coupon)">使用</view>
+		<view v-if="minusType == 1" class="c-card4" @click="$emit('useCoupon', coupon)">使用</view>
 
-    <!--未选中-->
-    <uni-icons v-if="minusType == 2" type="circle" class="c-card5" size="28" color="#BDBDBD" @click="$emit('useCoupon', coupon)" />
+		<!--未选中-->
+		<uni-icons v-if="minusType == 2" type="circle" class="c-card5" size="28" color="#BDBDBD"
+			@click="$emit('useCoupon', coupon)" />
 
-    <!-- 选中 -->
-    <uni-icons v-if="minusType == 3" type="circle-filled" class="c-card5" size="28" color="#3D49AB" @click="$emit('useCoupon', coupon)" />
+		<!-- 选中 -->
+		<uni-icons v-if="minusType == 3" type="circle-filled" class="c-card5" size="28" color="#3D49AB"
+			@click="$emit('useCoupon', coupon)" />
 
-  </view>
+	</view>
 </template>
 <script>
-export default {
-  name: "couponCard",
-  props: {
-    minusType: {
-      type: Number,
-      default: 1,  // 1: 使用    2: 未选中  3: 选中
-    },
-		coupon: {
-			id: 0,
-			couponNature: 1, // 1: 满减   2: 直减   3: 折扣
-			discount: 0, // 折扣
-			effectTime: '',  // 生效时间
-			expireTime: '',  // 失效时间
-			orderType:  0,  // 适用订单类型 1:实施/维修,2:勘测,3:人员,4:租赁,5:软件
-			parvalue: 0,  // 优惠券的面值(分)
-			publishId: 0, // 发行编号
-			remark: '', // 描述
-			title: '', // 标题
-			useMaxPrice: 0, // 使用最高限制价格条件（分）
-			useMinPrice:  0, // 使用最低价格条件（分）
+	export default {
+		name: "couponCard",
+		props: {
+			minusType: {
+				type: Number,
+				default: 1, // 1: 使用    2: 未选中  3: 选中
+			},
+			coupon: {
+				id: 0,
+				couponNature: 1, // 1: 满减   2: 直减   3: 折扣
+				discount: 0, // 折扣
+				effectTime: '', // 生效时间
+				expireTime: '', // 失效时间
+				orderType: 0, // 适用订单类型 1:实施/维修,2:勘测,3:人员,4:租赁,5:软件
+				parvalue: 0, // 优惠券的面值(分)
+				publishId: 0, // 发行编号
+				remark: '', // 描述
+				title: '', // 标题
+				useMaxPrice: 0, // 使用最高限制价格条件（分）
+				useMinPrice: 0, // 使用最低价格条件（分）
+			},
 		},
-  },
-  data() {
-    return {
-      spread: false
-    }
-  },
-  methods: {
-    changeSpread() {
-      this.spread = !this.spread
-    },
-		couponType(t) {
-			if (t ==1) {
-				return '满减';
-			} else if (t  ==  2) {
-				return '直减';
-			} else if (t == 3) {
-				return '折扣';
+		data() {
+			return {
+				spread: false
 			}
 		},
-		orderType(t) {
-			if (t ==1) {
-				return '实施/维修';
-			} else if (t  ==  2) {
-				return '勘测';
-			} else if (t == 3) {
-				return '人员';
-			} else if (t == 4) {
-				return '租赁';
-			} else if (t == 5) {
-				return '软件';
+		methods: {
+			changeSpread() {
+				this.spread = !this.spread
+			},
+			couponType(t) {
+				if (t == 1) {
+					return '满减';
+				} else if (t == 2) {
+					return '直减';
+				} else if (t == 3) {
+					return '折扣';
+				}
+			},
+			orderType(t) {
+				if (t == 1) {
+					return '实施/维修';
+				} else if (t == 2) {
+					return '勘测';
+				} else if (t == 3) {
+					return '人员';
+				} else if (t == 4) {
+					return '租赁';
+				} else if (t == 5) {
+					return '软件';
+				}else if (t == -1) {
+					return '全部';
+				}
 			}
 		}
-  }
-}
+	}
 </script>
 <style scoped lang="scss">
-.coupon-card {
-  display: flex;
-  width: 686rpx;
-  box-sizing: border-box;
-  min-height: 192rpx;
-  background: #FFF2DF;
-  border-radius: 16rpx;
-  padding: 0 20rpx;
+	.coupon-card {
+		display: flex;
+		width: 686rpx;
+		box-sizing: border-box;
+		min-height: 192rpx;
+		background: #FFF2DF;
+		border-radius: 16rpx;
+		padding: 0 20rpx;
 
-  .c-card1 {
-    align-self: center;
-    flex-shrink: 0;
-    text-align: center;
+		.c-card1 {
+			align-self: center;
+			flex-shrink: 0;
+			text-align: center;
 
-    .c-card1-money {
-      font-size: 52rpx;
-      font-family: PingFang SC-Medium, PingFang SC;
-      font-weight: 500;
-      color: #FF3B30;
-      line-height: 64rpx;
-    }
+			.c-card1-money {
+				font-size: 52rpx;
+				font-family: PingFang SC-Medium, PingFang SC;
+				font-weight: 500;
+				color: #FF3B30;
+				line-height: 64rpx;
+			}
 
-    .c-card1-mj {
-      margin-top: 16rpx;
-      font-size: 24rpx;
-      font-family: PingFang SC-Regular, PingFang SC;
-      font-weight: 400;
-      color: #FF3B30;
-      line-height: 32rpx;
-    }
-  }
+			.c-card1-mj {
+				margin-top: 16rpx;
+				font-size: 24rpx;
+				font-family: PingFang SC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #FF3B30;
+				line-height: 32rpx;
+			}
+		}
 
-  .c-card2 {
-    padding: 32rpx 0;
-    flex: 1;
-    margin-left: 26rpx;
+		.c-card2 {
+			padding: 32rpx 0;
+			flex: 1;
+			margin-left: 26rpx;
 
-    .c-card21 {
-      display: flex;
+			.c-card21 {
+				display: flex;
 
-      .c-card24 {
-        min-width: 64rpx;
-        height: 32rpx;
-        background: #34C759;
-        border-radius: 8rpx;
-        @include flexCenter;
-        font-size: 24rpx;
-        font-family: PingFang SC-Regular, PingFang SC;
-        font-weight: 400;
-        color: #FFFFFF;
-        line-height: 32rpx;
-        margin-right: 16rpx;
-      }
+				.c-card24 {
+					min-width: 64rpx;
+					height: 32rpx;
+					background: #34C759;
+					border-radius: 8rpx;
+					@include flexCenter;
+					font-size: 24rpx;
+					font-family: PingFang SC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #FFFFFF;
+					line-height: 32rpx;
+					margin-right: 16rpx;
+				}
 
-      .c-card24-red {
-        background: #FF3B30;
-      }
+				.c-card24-red {
+					background: #FF3B30;
+				}
 
-      .c-card25 {
-        font-size: 28rpx;
-        font-family: PingFang SC-Regular, PingFang SC;
-        font-weight: 400;
-        color: #323335;
-        line-height: 36rpx;
-      }
-    }
+				.c-card25 {
+					font-size: 28rpx;
+					font-family: PingFang SC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #323335;
+					line-height: 36rpx;
+				}
+			}
 
-    .c-card22 {
-      margin: 18rpx 0;
-      font-size: 24rpx;
-      font-family: PingFang SC-Regular, PingFang SC;
-      font-weight: 400;
-      color: #828282;
-      line-height: 32rpx;
-    }
+			.c-card22 {
+				margin: 18rpx 0;
+				font-size: 24rpx;
+				font-family: PingFang SC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #828282;
+				line-height: 32rpx;
+			}
 
-    .c-card23 {
-      @include flexBetween;
-      padding-right: 16rpx;
-    }
+			.c-card23 {
+				@include flexBetween;
+				padding-right: 16rpx;
+			}
 
-    .c-card23-text {
-      font-size: 20rpx;
-      font-family: PingFang SC-Medium, PingFang SC;
-      font-weight: 500;
-      color: #828282;
-      line-height: 28rpx;
-    }
-  }
+			.c-card23-text {
+				font-size: 20rpx;
+				font-family: PingFang SC-Medium, PingFang SC;
+				font-weight: 500;
+				color: #828282;
+				line-height: 28rpx;
+			}
+		}
 
-  .c-card3 {
-    flex-shrink: 0;
-    text-align: center;
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
+		.c-card3 {
+			flex-shrink: 0;
+			text-align: center;
+			display: flex;
+			flex-flow: column nowrap;
+			align-items: center;
 
-    .c-card31 {
-      width: 20rpx;
-      height: 10rpx;
-      background-color: white;
-      border-radius: 0 0 10rpx 10rpx;
-    }
+			.c-card31 {
+				width: 20rpx;
+				height: 10rpx;
+				background-color: white;
+				border-radius: 0 0 10rpx 10rpx;
+			}
 
-    .c-card32 {
-      height: calc(100% - 20rpx);
-      width: 0;
-      border-left: 2rpx dashed white;
-    }
+			.c-card32 {
+				height: calc(100% - 20rpx);
+				width: 0;
+				border-left: 2rpx dashed white;
+			}
 
-    .c-card33 {
-      width: 20rpx;
-      height: 10rpx;
-      background-color: white;
-      border-radius: 10rpx 10rpx 0 0;
-    }
-  }
+			.c-card33 {
+				width: 20rpx;
+				height: 10rpx;
+				background-color: white;
+				border-radius: 10rpx 10rpx 0 0;
+			}
+		}
 
-  .c-card4 {
-    flex-shrink: 0;
-    margin-left: 8rpx;
-    align-self: center;
-    width: 116rpx;
-    height: 64rpx;
-    background: linear-gradient(181deg, #3340A0 0%, #2C3580 94%);
-    border-radius: 32rpx;
-    @include flexCenter;
-    font-size: 28rpx;
-    font-family: PingFang SC-Regular, PingFang SC;
-    font-weight: 400;
-    color: #FFFFFF;
-  }
+		.c-card4 {
+			flex-shrink: 0;
+			margin-left: 8rpx;
+			align-self: center;
+			width: 116rpx;
+			height: 64rpx;
+			background: linear-gradient(181deg, #3340A0 0%, #2C3580 94%);
+			border-radius: 32rpx;
+			@include flexCenter;
+			font-size: 28rpx;
+			font-family: PingFang SC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #FFFFFF;
+		}
 
-  .c-card5 {
-    align-self: center;
-    padding: 0 12rpx 0 10rpx;
-  }
-}
+		.c-card5 {
+			align-self: center;
+			padding: 0 12rpx 0 10rpx;
+		}
+	}
 </style>
--- ttd-app/pages/mine/myCoupons/myCoupons.vue
+++ ttd-app/pages/mine/myCoupons/myCoupons.vue
@@ -1,154 +1,165 @@
 <template>
-  <view>
-    <custom-navbar title="我的优惠券" />
+	<view>
+		<custom-navbar title="我的优惠券" />
 
-    <back-container>
-			
+		<back-container>
+
 			<template v-slot:headerSlot>
-			  <blue-tab v-if="!isSelect" :active-key="activeKey" :list="tabList" @change="change" />
+				<blue-tab v-if="!isSelect" :active-key="activeKey" :list="tabList" @change="change" />
 			</template>
 
-      <view class="my-coupon">
-        <view class="my-coupon-item" v-for="(item, i) in couponsList" :key="i">
-          <coupon-card 
-					:coupon="item" 
-					@useCoupon="useCoupon"
-					:minus-type="couponType(item)"
-					/>
-        </view>
+			<view class="my-coupon">
+				<view class="my-coupon-item" v-for="(item, i) in couponsList" :key="i">
+					<coupon-card :coupon="item" @useCoupon="useCoupon" :minus-type="couponType(item)" />
+				</view>
 
-        <list-empty v-if="!couponsList.length" />
-      </view>
-    </back-container>
-		
+				<list-empty v-if="!couponsList.length" />
+			</view>
+		</back-container>
+
 		<iphonex-bottom v-if="isSelect">
-		  <big-btn button-text="确定" @click="toSelect"/>
+			<big-btn button-text="确定" @click="toSelect" />
 		</iphonex-bottom>
-  </view>
+	</view>
 </template>
 
 <script>
-import BackContainer from "../addressManage/component/backContainer";
-import BlueTab from "../addressManage/component/blueTab";
-import CouponCard from "./children/couponCard";
-import ListEmpty from "../../place-order/orderList/listEmpty";
-import IphonexBottom from "../addressManage/component/iphonexBottom";
-import BigBtn from "../addressManage/component/bigBtn";
+	import BackContainer from "../addressManage/component/backContainer";
+	import BlueTab from "../addressManage/component/blueTab";
+	import CouponCard from "./children/couponCard";
+	import ListEmpty from "../../place-order/orderList/listEmpty";
+	import IphonexBottom from "../addressManage/component/iphonexBottom";
+	import BigBtn from "../addressManage/component/bigBtn";
 
-export default {
-  name: "myCoupons",
-  components: { ListEmpty, CouponCard, BlueTab, BackContainer, BigBtn, IphonexBottom },
-  data() {
-    return {
-      activeKey: '0',
-      tabList: [
-        { text: '未使用', key: '0' },
-        { text: '已使用', key: '1' },
-        { text: '已过期', key: '3' },
-      ],
-			couponsList: [],
-			isSelect: false,
-			selectList: [],
-			minUsePrice: 0,
-			orderType: 0,
-			state: 0,
-    };
-  },
-	onLoad(option) {
-		if (option.isSelect) {
-			this.isSelect = option.isSelect == '1';
-		}
-		if (option.minUsePrice) {
-			this.minUsePrice = Number(option.minUsePrice);
-		}
-		if (option.orderType) {
-			this.orderType = Number(option.orderType);
-		}
-		if (option.state) {
-			this.state = Number(option.state);
-		}
-		const eventChannel = this.getOpenerEventChannel();
-		eventChannel.on('selectCoupon', (coupon) => {
-			if (coupon.id) {
-				this.selectList = [coupon];
-			}
-		});
-	},
-	onReady() {},
-	onShow() {
-		this.queryCouponsList();
-	},
-  methods: {
-    change(data) {
-      this.activeKey = data;
-			this.queryCouponsList();
-    },
-		queryCouponsList() {
-			const params = { 
-				minUsePrice: this.isSelect ? this.minUsePrice : undefined,
-				orderType: this.isSelect ? this.orderType : undefined,
-				state: this.isSelect ? this.state : this.activeKey, 
-			  pageSize: 100,
-				sortInfos: [{
-					field: 'parvalue',
-					sort: 'desc',
-				}]
+	export default {
+		name: "myCoupons",
+		components: {
+			ListEmpty,
+			CouponCard,
+			BlueTab,
+			BackContainer,
+			BigBtn,
+			IphonexBottom
+		},
+		data() {
+			return {
+				activeKey: '0',
+				tabList: [{
+						text: '未使用',
+						key: '0'
+					},
+					{
+						text: '已使用',
+						key: '1'
+					},
+					{
+						text: '已过期',
+						key: '3'
+					},
+				],
+				couponsList: [],
+				isSelect: false,
+				selectList: [],
+				minUsePrice: 0,
+				orderType: 0,
+				state: 0,
 			};
-			this.$http.post('/b/coupon/queryPageList', params, true)
-			.then(res => {
-			  this.couponsList = res.dataList;
-			})
 		},
-		couponType(coupon) {
-			if (!this.isSelect) {
-				return 1;
-			} else {
-				const index = this.selectList.findIndex((c)  => c.id == coupon.id);
-				return index == -1 ? 2 : 3;
+		onLoad(option) {
+			if (option.isSelect) {
+				this.isSelect = option.isSelect == '1';
 			}
-		},
-		useCoupon(coupon) {
-			if (this.isSelect) {
-				this.checkSelect(coupon);
-			} else {
-				uni.switchTab({
-				    url: '/pages/place-order/index/index'
-				});
+			if (option.minUsePrice) {
+				this.minUsePrice = Number(option.minUsePrice);
 			}
-		},
-		checkSelect(coupon) {
-			const index = this.selectList.findIndex((c)  => c.id == coupon.id);
-			if (index != -1) {
-				this.selectList = [];
-			} else {
-				this.selectList = [coupon];
+			if (option.orderType) {
+				this.orderType = Number(option.orderType);
 			}
-		},
-		toSelect() {
-			const coupon = this.selectList[0];
+			if (option.state) {
+				this.state = Number(option.state);
+			}
 			const eventChannel = this.getOpenerEventChannel();
-			eventChannel.emit('onSelect', coupon ? coupon : {});
-			
-			uni.navigateBack({});
+			eventChannel.on('selectCoupon', (coupon) => {
+				if (coupon.id) {
+					this.selectList = [coupon];
+				}
+			});
 		},
-  }
-}
+		onReady() {},
+		onShow() {
+			this.queryCouponsList();
+		},
+		methods: {
+			change(data) {
+				this.activeKey = data;
+				this.queryCouponsList();
+			},
+			queryCouponsList() {
+				const params = {
+					minUsePrice: this.isSelect ? this.minUsePrice : undefined,
+					orderType: this.isSelect ? this.orderType : undefined,
+					state: this.isSelect ? this.state : this.activeKey,
+					pageSize: 100,
+					sortInfos: [{
+						field: 'parvalue',
+						sort: 'desc',
+					}]
+				};
+				this.$http.post('/b/coupon/queryPageList', params, true)
+					.then(res => {
+						this.couponsList = res.dataList;
+					})
+			},
+			couponType(coupon) {
+				if (!this.isSelect) {
+					return 1;
+				} else {
+					const index = this.selectList.findIndex((c) => c.id == coupon.id);
+					return index == -1 ? 2 : 3;
+				}
+			},
+			useCoupon(coupon) {
+				if (this.isSelect) {
+					this.checkSelect(coupon);
+				} else {
+					uni.switchTab({
+						url: '/pages/place-order/index/index'
+					});
+				}
+			},
+			checkSelect(coupon) {
+				const index = this.selectList.findIndex((c) => c.id == coupon.id);
+				if (index != -1) {
+					this.selectList = [];
+				} else {
+					this.selectList = [coupon];
+				}
+			},
+			toSelect() {
+				const coupon = this.selectList[0];
+				const eventChannel = this.getOpenerEventChannel();
+				eventChannel.emit('onSelect', coupon ? coupon : {});
+
+				uni.navigateBack({});
+			},
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
-.my-coupon {
-  .my-coupon-item {
-    box-sizing: border-box;
-    margin-left: 32rpx;
-    border-bottom: 1rpx solid #F6F6F6;
-    padding: 32rpx 32rpx 32rpx 0;
+	.my-coupon {
+		.my-coupon-item {
+			box-sizing: border-box;
+			margin-left: 32rpx;
+			border-bottom: 1rpx solid #F6F6F6;
+			padding: 32rpx 32rpx 32rpx 0;
 
-    &:nth-last-child(1) {
-      border: none;
-    }
-  }
-}
+			&:nth-last-child(1) {
+				border: none;
+			}
+		}
+	}
 </style>
 <style lang="scss">
-@import "../addressManage/_pageStyle.scss";
+	@import "../addressManage/_pageStyle.scss";
 </style>
